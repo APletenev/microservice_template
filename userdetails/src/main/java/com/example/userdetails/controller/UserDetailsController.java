1c8e9739f744f5b43e88fc0206d9cc9a35d6fde9
Andrey Pletenev <pletenev.andrey@yandex.ru>
2023-03-24 15:33:49 +0700

    User details: endpoint Админ может просматривать информацию о пользователях

--- userdetails/src/main/java/com/example/userdetails/controller/UserDetailsController.java
+++ userdetails/src/main/java/com/example/userdetails/controller/UserDetailsController.java
@@ -1,13 +1,28 @@
 package com.example.userdetails.controller;
 
+import com.example.userdetails.entity.UserDetails;
+import com.example.userdetails.service.UserDetailsService;
+import lombok.AllArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/api/userdetails")
 @RestController
+@AllArgsConstructor
 public class UserDetailsController {
+    private UserDetailsService userDetailService;
 
-    @GetMapping("/userdetails")
-    public String getUserdetails() {
-        return "balance, email";
+    /**
+     * Админ может просматривать информацию о пользователях
+     * @param username имя пользователя, информацию по которому нужно посмотреть
+     * @return email пользователя и его текущий баланс
+     */
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/{username}")
+    public UserDetails getUserdetails(@PathVariable String username) {
+        return userDetailService.getDetailsByName(username);
     }
 }
