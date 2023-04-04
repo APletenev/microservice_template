b64a40c10c9f443ab4fbc352ccff6a3383353037
Andrey Pletenev <pletenev.andrey@yandex.ru>
2023-04-04 11:48:18 +0700

    Identity provider: sample passwords bcrypt encoded

--- idp/src/main/java/com/example/idp/SampleUsersLoader.java
+++ idp/src/main/java/com/example/idp/SampleUsersLoader.java
@@ -17,6 +17,7 @@ import java.util.logging.Logger;
 public class SampleUsersLoader {
 
     private JdbcUserDetailsManager jdbcUserDetailsManager;
+
     private final Logger log = Logger.getLogger(this.getClass().getName());
 
     @EventListener(ApplicationReadyEvent.class)
@@ -24,9 +25,9 @@ public class SampleUsersLoader {
         log.info("Development environment preparing. Loading sample data..");
         try {
             jdbcUserDetailsManager.createUser(
-                    User.withUsername("admin").password("{noop}12345").roles("ADMIN").build());
+                    User.withUsername("admin").password("{bcrypt}"+"$2a$12$z0XvKwsTBcObssE6KffWmuekgfrnEs0wiI4BRAqtsK/M4lKGdJrWS").roles("ADMIN").build());
             jdbcUserDetailsManager.createUser(
-                    User.withUsername("user").password("{noop}67890").roles("USER").build());
+                    User.withUsername("user").password("{bcrypt}"+"$2a$12$UNZp13hRqxsdLVhyxMqVE.XfEwM/H6A2p8CzHM7dHp4VFcUxpJbuu").roles("USER").build());
         } catch (DuplicateKeyException e) {
             log.info("users already exist");
         }
