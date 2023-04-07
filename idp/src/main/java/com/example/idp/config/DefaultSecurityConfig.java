37e79660432fc3e3b701c9a31303477989e45bb6
Andrey Pletenev <pletenev.andrey@yandex.ru>
2023-04-07 10:59:00 +0700

    Identity provider: minor - removed obsolete configureGlobal

--- idp/src/main/java/com/example/idp/config/DefaultSecurityConfig.java
+++ idp/src/main/java/com/example/idp/config/DefaultSecurityConfig.java
@@ -2,7 +2,6 @@ package com.example.idp.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.Authentication;
@@ -23,13 +22,6 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class DefaultSecurityConfig {
     @Autowired
     private DataSource dataSource;
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth)
-            throws Exception {
-        auth.jdbcAuthentication()
-                .dataSource(dataSource);
-    }
 
     @Bean
     public JdbcUserDetailsManager jdbcUserDetailsManager()
