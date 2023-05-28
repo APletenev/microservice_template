e871fe1606efb4f86298232edc90940ea5b85824
Andrey Pletenev <pletenev.andrey@yandex.ru>
2023-05-28 16:14:06 +0700

    E2E tests updated for signup

--- e2e-tests/src/test/java/com/example/PrepareTest.java
+++ e2e-tests/src/test/java/com/example/PrepareTest.java
@@ -1,11 +1,18 @@
 package com.example;
 
 
+import com.example.common.coreapi.UserDTO;
 import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.RequestOptions;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.TestInstance;
 
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SuppressWarnings("SpellCheckingInspection")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class PrepareTest {
 
@@ -14,6 +21,8 @@ public class PrepareTest {
     BrowserContext context;
     APIRequestContext request;
     final String gatewayURL = "https://" + System.getenv("GATEWAY_HOST") + ":" + System.getenv("GATEWAY_PORT");
+    final String idpURL = "http://" + System.getenv("IDP_HOST") + ":" + System.getenv("IDP_PORT");
+    private String username;
 
     @BeforeAll
     void launchBrowser() {
@@ -21,11 +30,19 @@ public class PrepareTest {
         browser = playwright.chromium().launch();
         context = browser.newContext(new Browser.NewContextOptions().setIgnoreHTTPSErrors(true));
         request = context.request();
+        username = "testuser" + UUID.randomUUID();
+
+        unauthorizedCanSignUp(); //Create user and account for using by another tests
     }
 
     @AfterAll
     void closeBrowser() {
         playwright.close();
+    }
+
+    void unauthorizedCanSignUp() {
+        APIResponse response=signUp();
+        assertEquals(response.status(),200);
     }
 
     Page login(String username, String password) {
@@ -44,7 +61,21 @@ public class PrepareTest {
         return request.get(
                 gatewayURL +
                         System.getenv("ACCOUNT_API") +
-                        "/user");
+                        "/"+ this.username);
+    }
+
+    APIResponse signUp() {
+        return request.post(
+                idpURL +
+                        System.getenv("IDP_API") +
+                        "/signup", RequestOptions.create().setData(new UserDTO(this.username, "testpassword", "test@email.com")));
+    }
+
+    APIResponse tryEndPointOtherThenSignup() {
+        return request.get(
+                idpURL +
+                        System.getenv("IDP_API") +
+                        "/");
     }
 
 }
