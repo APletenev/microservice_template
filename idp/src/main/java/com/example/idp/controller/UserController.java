b137e39500118eca1b2968ea474ed2e2cbcbbb99
Andrey Pletenev <pletenev.andrey@yandex.ru>
2023-07-04 13:40:49 +0700

    Signup endpoint constant introduced

--- idp/src/main/java/com/example/idp/controller/UserController.java
+++ idp/src/main/java/com/example/idp/controller/UserController.java
@@ -5,25 +5,29 @@ import com.example.common.coreapi.commands.SignupUserCommand;
 import lombok.AllArgsConstructor;
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.axonframework.queryhandling.QueryGateway;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
+
+import static com.example.common.ApiEndpoints.SIGNUP_ENDPOINT;
 
 @RequestMapping("#{environment.IDP_API}")
 @RestController
 @AllArgsConstructor
 public class UserController {
 
-//    private UserService userService;
-
     private CommandGateway commandGateway;
 
-    private QueryGateway queryGateway;
-
-    @PostMapping("/signup")
-    public CompletableFuture<Void> signUpUser(@RequestBody UserDTO credentials) {
-        return commandGateway.send(new SignupUserCommand(UUID.randomUUID().toString(),credentials.getUsername(), credentials.getPassword(), credentials.getEmail()));
+    @PostMapping(SIGNUP_ENDPOINT)
+        public CompletableFuture<ResponseEntity<String>> signUpUser(@RequestBody UserDTO credentials) {
+        return commandGateway.send(new SignupUserCommand(UUID.randomUUID().toString(), credentials.getUsername(), credentials.getPassword(), credentials.getEmail()))
+                .thenApply(result -> new ResponseEntity<>("User created successfully", HttpStatus.CREATED));
     }
 
 }
