7907c1288f94a970825565170b764c1769fc12a1
Andrey Pletenev <pletenev.andrey@yandex.ru>
2023-07-17 14:47:51 +0700

    Replaced binder from RabbitMQ to Kafka

--- common/src/main/java/com/example/common/coreapi/Credentials.java
+++ common/src/main/java/com/example/common/coreapi/Credentials.java
@@ -1,8 +1,10 @@
 package com.example.common.coreapi;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class Credentials {
     private final String username;
     private final String password;
