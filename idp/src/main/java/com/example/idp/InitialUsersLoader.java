f0c97d458f1a8c8d5dce8efadda03e2a44140b04
Andrey Pletenev <pletenev.andrey@yandex.ru>
2023-08-07 12:42:34 +0700

    Improved initial users loader with environment variables usage

--- idp/src/main/java/com/example/idp/InitialUsersLoader.java
+++ idp/src/main/java/com/example/idp/InitialUsersLoader.java
@@ -5,8 +5,10 @@ import lombok.AllArgsConstructor;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.env.Environment;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.stereotype.Component;
 
@@ -15,9 +17,11 @@ import java.util.logging.Logger;
 @Component
 @Profile("dev")
 @AllArgsConstructor
-public class SampleUsersLoader {
+public class InitialUsersLoader {
 
     private JdbcUserDetailsManager jdbcUserDetailsManager;
+
+    private Environment env;
 
     private final Logger log = Logger.getLogger(this.getClass().getName());
 
@@ -26,7 +30,9 @@ public class SampleUsersLoader {
         log.info("Development environment preparing. Loading sample data..");
         try {
             jdbcUserDetailsManager.createUser(
-                    User.withUsername("admin").password("{bcrypt}"+"$2a$12$z0XvKwsTBcObssE6KffWmuekgfrnEs0wiI4BRAqtsK/M4lKGdJrWS").roles(Roles.ADMIN_ROLE).build());
+                    User.withUsername(env.getProperty("IDP_ADMIN")).password(
+                            "{bcrypt}" + BCrypt.hashpw(env.getProperty("IDP_ADMIN_PASSWORD"), BCrypt.gensalt())).roles(
+                            Roles.ADMIN_ROLE).build());
         } catch (DuplicateKeyException e) {
             log.info("users already exist");
         }
